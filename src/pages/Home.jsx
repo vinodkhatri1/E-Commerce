d27a8addae7a304a7b933764801a16e047bff484
Vinod kumar <vinodkkhatri12@gmail.com>
2026-01-21 16:04:18 +0500

    improve header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import Nav from "../component/Nav";
+import ImageSlider from "../component/ImageSlider";
 import ContainerRules from "../component/ContainerRules";
 import TrandingProduct from "../component/TrandingProduct";
 import Banner from "../component/Banner";
 import NewArrivals from "../component/NewArrivals";
 
 const Home = () => {
-    return (
-        <div className="w-full overflow-hidden">
-            <Nav />
-            <ContainerRules />
-            <TrandingProduct />
-            <Banner />
-            <NewArrivals />
-        </div>
-    )
-}
-export default Home
+  return (
+    <div className="w-full overflow-hidden">
+      <ImageSlider />
+      <ContainerRules />
+      <TrandingProduct />
+      <Banner />
+      <NewArrivals />
+    </div>
+  );
+};
+export default Home;
